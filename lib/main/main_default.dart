846543c6c23e2d499959d6c6675d55cb70548e3a
Rajesh Patil <xrajeshpatil@moneytap.com>
2021-08-03 00:20:01 +0530

    code refactored

--- lib/main/main_default.dart
+++ lib/main/main_default.dart
@@ -2,14 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:doctor_bima/analytics/analytics_manager.dart';
-import 'package:doctor_bima/analytics/sentry.dart';
 import 'package:doctor_bima/appStateContainer/app_state_container.dart';
 import 'package:doctor_bima/bloc/bloc_observer.dart';
 import 'package:doctor_bima/di/di_initializer.dart';
 import 'package:doctor_bima/main/app.dart';
 import 'dart:async';
 import 'package:doctor_bima/native/platform_utils.dart';
-// import 'package:sentry/sentry.dart';
 
 defaultMain() {
   FlutterError.onError = (FlutterErrorDetails details) async {
@@ -28,7 +26,6 @@ defaultMain() {
     Bloc.observer = DBBlocObserver();
     runApp(StateContainer(child: LaunchApp()));
   }, (Object error, StackTrace stackTrace) async {
-    // final SentryClient sentry = await Sentry.initSentryClient();
-    // Sentry.reportError(sentry, error, stackTrace);
+    //Report error if any
   });
 }
