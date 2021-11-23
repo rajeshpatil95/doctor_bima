2169ad2f4e83083d21474c7837cdf0741708278f
Rajesh Patil <you@example.com>
2021-11-23 20:47:31 +0530

    Added commonsUtils methods

--- integration_test/constants/test_constants.dart
+++ integration_test/constants/test_constants.dart
@@ -1,1 +1,7 @@
-class TestConstants {}
+import 'dart:convert';
+
+class TestConstants {
+  static const prettyEncoder = JsonEncoder.withIndent('  ');
+  static const testOutputFilename = 'integration_response_data';
+  static const destinationDirectory = 'integration_test/testData';
+}
--- integration_test/utils/common_utils.dart
+++ integration_test/utils/common_utils.dart
@@ -1,1 +1,7 @@
-class CommonUtils {}
+import '../constants/test_constants.dart';
+
+class CommonUtils {
+  static String encodeJson(Map<String, dynamic> jsonObject) {
+    return TestConstants.prettyEncoder.convert(jsonObject);
+  }
+}
